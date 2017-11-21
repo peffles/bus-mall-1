97964268179c9b2129d123ffdb76524016893163
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-11-20 16:12:08 -0800

    add images

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'strict';
+
+var allProducts = [];
+var productNames = ['boots', 'chair', 'scissors']; // TODO: see the pattern here, and what you need to fill in?
+function Product(productName, clickCount, displayCount) {
+  this.productName = productName;
+  this.clickCount = clickCount;
+  this.displayCount = displayCount;
+  allProducts.push(this);
+}
+
+document.getElementById('image-one').addEventListener('click', function() {
+  var xx = 3;
+  console.log(xx);
+}, false);
