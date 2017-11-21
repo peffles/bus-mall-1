af75d54f3c2174222033b146725a917fc5767a7e
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-11-20 16:44:24 -0800

    add object build

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 'strict';
 
 var allProducts = [];
-var productNames = ['boots', 'chair', 'scissors']; // TODO: see the pattern here, and what you need to fill in?
-function Product(productName, clickCount, displayCount) {
+var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var fileNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+function Product(productName, fileName, clickCount, displayCount) {
   this.productName = productName;
+  this.fileName = fileName;
   this.clickCount = clickCount;
   this.displayCount = displayCount;
   allProducts.push(this);
 }
+
+for(var i = 0; i < productNames.length; i++) {
+  new Product(productNames[i], fileNames[i], 0, 0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementById('image-one').addEventListener('click', function() {
   var xx = 3;
